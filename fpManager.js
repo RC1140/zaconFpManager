c6d892e26acce8ed6f5236452746ddbda98a6889
jameel <jameel@republiccommandos.co.za>
2011-06-06 08:56:34 +0200

    Changes for dot cloud

--- fpManager.js
+++ fpManager.js
@@ -10,7 +10,7 @@ fpManager.fpCurrentWiningUser = '';
 var milliseconds = 3600000;
 
 var setupNextFpOpen = function(ircClient){
-   var timeToNextFp = (Math.round(((Math.random() * 24)) * 1)/1) * 10000;// *milliseconds;
+   var timeToNextFp = (Math.round(((Math.random() * 24)) * 1)/1) * milliseconds ;//10000;
    fpManager.fpOpen = false;
    setTimeout(function(){
         announceAndOpenFp(ircClient);      
