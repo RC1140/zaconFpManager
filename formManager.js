2ebe0d5db02f98ab2f37eb0fb0a054faa4454745
jameel <jameel@republiccommandos.co.za>
2011-06-11 10:18:28 +0200

    Initial Checkin

---
+++ formManager.js
@@ -0,0 +1,71 @@
+var express  = require('express');
+var Recaptcha = require('recaptcha').Recaptcha;
+var config = require('./settings');
+var fpMan = require('./fpManager');
+var app = express.createServer();
+
+var formManager = {}
+var client;
+
+app.configure(function() {
+    app.use(express.bodyParser());
+});
+
+var initialDisplayForUser = function(req, res) {
+    if (req.params.hash == fpMan.currentHash){
+        var recaptcha = new Recaptcha(config.recaptcha.PUBLIC_KEY, config.recaptcha.PRIVATE_KEY);
+
+        res.render('form.jade', {
+            layout: false,
+            locals: {
+                recaptcha_form: recaptcha.toHTML()
+            }
+        });
+    }else{
+       res.send('Invalid URL or timeout expired'); 
+    };
+};
+
+var handleHumanVerification = function(req, res) {
+    if (req.params.hash == fpMan.currentHash){
+        var data = {
+            remoteip:  req.connection.remoteAddress,
+            challenge: req.body.recaptcha_challenge_field,
+            response:  req.body.recaptcha_response_field
+        };
+        var recaptcha = new Recaptcha(config.recaptcha.PUBLIC_KEY, config.recaptcha.PRIVATE_KEY, data);
+
+        recaptcha.verify(function(success, error_code) {
+            if (success) {
+               //If the user if valid , record a success for the name entered , or based on the url
+               fpMan.currentHash = '';
+               res.send('You have won this FP , you are hopefully human thanks for helping');
+               client.say(config.IRC.channel,fpMan.fpCurrentWiningUser +' has won the fp.');
+               fpMan.userHasWon(fpMan.fpCurrentWiningUser);
+               fpMan.fpCurrentWiningUser = ''; 
+               fpMan.setupNextFpOpen(client);
+            }
+            else {
+                // Redisplay the form.
+                res.render('form.jade', {
+                    layout: false,
+                    locals: {
+                        recaptcha_form: recaptcha.toHTML()
+                    }
+                });
+            }
+        });
+    }else{
+       res.send('Invalid URL or timeout expired'); 
+    };
+};
+
+formManager.loadAndInit = function(ircClient){
+    client = ircClient;
+    //Setup the url handles and start listening
+    app.post('/:hash', handleHumanVerification);
+    app.get('/:hash',initialDisplayForUser );
+    app.listen(8080);
+};
+
+module.exports = formManager;
---
+++ server.js
@@ -0,0 +1,4 @@
+//This is all that is really required since the client auto connects
+process.env.TZ = 'Africa/Johannesburg'
+var ircMan = require('./ircManager');
+
